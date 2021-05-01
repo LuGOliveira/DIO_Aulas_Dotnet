0ea86be0db9ae9e4f72395b52c564e4a2573d0a2
LuGOliveira <contatolgo.ti@gmail.com>
2021-05-01 13:44:50 -0300

    Criando uma aplicação console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO_Aula_Dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
