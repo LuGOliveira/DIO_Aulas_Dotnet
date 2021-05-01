dfd34cffb93c8a99187dc012a81c4103862d2f9e
LuGOliveira <contatolgo.ti@gmail.com>
2021-05-01 14:36:38 -0300

    Aula 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace DIO_Aula_Dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nVezes = 5;
+            for (int i=0; i<nVezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .Net {i}");
+            }
         }
     }
 }
